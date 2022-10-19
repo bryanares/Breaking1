2eeb9a7d220e01573db204ef28751a54d0597110
bryanares <bryanares12@gmail.com>
2022-10-19 09:01:50 +0300

    new activity declaration

---
+++ app/src/main/java/com/brian/breakingbad/CharacterDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.brian.breakingbad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CharacterDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_detail)
+    }
+}
